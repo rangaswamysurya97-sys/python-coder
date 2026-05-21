f18fc0fe08f6700d2b4ca1f36574ad1fa3b96bea
SURYA <rangaswamysurya97@gmail.com>
2026-05-21 10:06:54 +0530

    task45

--- password_checker.py
+++ password_checker.py
@@ -1,48 +1,14 @@
 import re
 
-def check_password_strength(password):
+password = input("Enter password: ")
 
-    score = 0
+length = len(password) >= 8
+upper = re.search(r"[A-Z]", password)
+lower = re.search(r"[a-z]", password)
+digit = re.search(r"\d", password)
+special = re.search(r"[@$!%*?&]", password)
 
-    # Minimum length check
-    if len(password) >= 8:
-        score += 1
-
-    # Uppercase letter check
-    if re.search(r"[A-Z]", password):
-        score += 1
-
-    # Lowercase letter check
-    if re.search(r"[a-z]", password):
-        score += 1
-
-    # Number check
-    if re.search(r"[0-9]", password):
-        score += 1
-
-    # Special character check
-    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
-        score += 1
-
-    # Password strength result
-    print("\n===== Password Strength Report =====\n")
-
-    if score == 5:
-        print("Strength: VERY STRONG")
-    elif score == 4:
-        print("Strength: STRONG")
-    elif score == 3:
-        print("Strength: MEDIUM")
-    elif score == 2:
-        print("Strength: WEAK")
-    else:
-        print("Strength: VERY WEAK")
-
-    print(f"Score: {score}/5")
-
-
-# User input
-password = input("Enter your password: ")
-
-# Check password strength
-check_password_strength(password)
+if length and upper and lower and digit and special:
+    print("Strong Password")
+else:
+    print("Weak Password")
