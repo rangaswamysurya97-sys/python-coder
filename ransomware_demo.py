098a8b60ea110ef4c1911bd17f570f73eb8be0d1
SURYA <rangaswamysurya97@gmail.com>
2026-05-21 10:15:25 +0530

    task50

---
+++ ransomware_demo.py
@@ -0,0 +1,13 @@
+files = [
+    "photo.jpg",
+    "notes.txt",
+    "project.docx"
+]
+
+print("Simulating File Encryption...")
+
+for file in files:
+
+    print(f"{file} -> ENCRYPTED")
+
+print("Demo Complete")
